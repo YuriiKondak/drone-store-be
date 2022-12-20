539481ad5ea94f1272791d2a27611208de0f70c7
Yurii Kondak <YuriiKondak>
2022-12-21 01:04:11 +0200

    add changes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,14 +4,16 @@ class Users::SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
     session[:user_id] = resource.id
+
     render json: { user: resource, auth_token: current_token }, status: :ok
   end
 
-  private
+  protected
 
   def respond_with(resource, _opts = {})
     render json: { message: 'Logged.'}, status: :ok
   end
+
   def respond_to_on_destroy
     current_user ? log_out_success : log_out_failure
   end
